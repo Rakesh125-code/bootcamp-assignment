aa7926aca8a65d0357ff4ccaef5c739e930a4ed6
Rakesh Kumar <56781805+Rakesh125-code@users.noreply.github.com>
2024-01-20 16:44:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/api/task3', async (req, res) => {
 
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin",  "http://localhost:3000");
+  //res.header("Access-Control-Allow-Origin",  "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
